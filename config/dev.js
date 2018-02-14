eaa82df8cf06f9898fd06c957486d61648fefacd
fantomaz <romulo_fantomaz@hotmail.com>
2018-02-13 22:20:42 -0200

    Dev variables file included

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: /* This value will be the Client ID created by Google */,
+    googleClientSecret: /* This value will be the Client Secret created by Google */,
+    mongoURI: /* This value will be the standard MongoDB URI  */,
+    cookieKey: /* This value can be any string */
+};
